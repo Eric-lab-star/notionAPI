230795c8ddf746a9aa610133c1f4b083ea374672
Eric <cyon256@icloud.com>
2022-02-04 23:06:26 +0900

    create page

--- public/main.js
+++ public/main.js
@@ -1,22 +1,23 @@
-console.log("heelo");
 const div = document.getElementById("div");
 
-const getInfos = async () => {
+/* const getInfos = async () => {
   const res = await fetch("http://localhost:4000/infos");
   const data = await res.json();
+  console.log(data);
   return data;
 };
-
-const addData = async () => {
+getInfos(); */
+/* const addData = async () => {
   const infos = await getInfos();
   infos.forEach((info) => {
     const notionInfo = document.createElement("div");
     notionInfo.innerHTML = `
             <h3>${info.Name}</h3>
+              <ul>
+                <li>${info.Date}</li>
+              </ul>
 
         `;
     div.appendChild(notionInfo);
   });
-};
-
-addData();
+}; */
--- src/app.js
+++ src/app.js
@@ -1,42 +1,84 @@
 import express from "express";
 import "dotenv/config";
-import { Client } from "@notionhq/client";
-
+import { Client, LogLevel } from "@notionhq/client";
+const checkboxId = process.env.CHECKBOX;
+const dateId = process.env.DATE;
+const desId = process.env.DESCRIPTION;
+const titleId = process.env.TITLE;
+const numberID = process.env.NUMBER;
+const database_id = process.env.DATABASE_ID;
+const newDate = new Date();
 const app = express();
 
 //init client
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
+  logLevel: LogLevel.DEBUG,
 });
-const database_id = process.env.DATABASE_ID;
+
 //
-
-const notionData = async () => {
-  const { results } = await notion.databases.query({ database_id });
-  const infos = results.map((a) => {
-    return {
-      id: a.id,
-      Date: a.properties.Date.date.start,
-      Name: a.properties.Name.title[0].text.content,
-    };
-  });
-  return infos;
-
-  /* const payload = {
-    path: `databases/${database_id}/query`,
-    method: `POST`,
-  };
-  const { results } = await notion.request(payload);
-
-  const infos = results.map((a) => {
-    return {
-      id: a.id,
-      Date: a.properties.Date.date.start,
-      Name: a.properties.Name.title[0].text.content,
-    };
-  });
-  return infos; */
+const getDatabase = async () => {
+  const response = await notion.databases.retrieve({ database_id });
+  console.log(notionPropertiesById(response.properties));
 };
+
+getDatabase();
+
+const reducer = (obj, property) => {
+  const { id, ...rest } = property;
+  return { ...obj, [id]: rest };
+};
+
+function notionPropertiesById(properties) {
+  return Object.values(properties).reduce(reducer, {});
+}
+
+const createPage = async ({ title, description, isTrue, number }) => {
+  const page = await notion.pages.create({
+    parent: { database_id },
+    properties: {
+      [titleId]: {
+        title: [
+          {
+            type: "text",
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      [desId]: {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: description,
+            },
+          },
+        ],
+      },
+      [dateId]: {
+        date: {
+          start: newDate.toISOString(),
+        },
+      },
+      [checkboxId]: {
+        checkbox: isTrue,
+      },
+      [numberID]: {
+        number: number,
+      },
+    },
+  });
+  return page;
+};
+createPage({
+  title: "test",
+  description: "notion integration",
+  isTrue: true,
+  number: 1,
+});
+
 //
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
@@ -47,10 +89,10 @@ app.get("/", async (req, res) => {
   res.render("base");
 });
 //
-app.get("/infos", async (req, res) => {
-  const datas = await notionData();
+/* app.get("/infos", async (req, res) => {
+  const datas = await getDatabase();
   res.json(datas);
-});
+}); */
 
 app.listen(4000, () => {
   console.log(`http://localhost:4000`);
